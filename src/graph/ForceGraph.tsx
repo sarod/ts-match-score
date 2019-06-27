ee6d7a3e7bf4b3e6dca739cb157194447bf7d4a7
Sebastien Arod <sbnarod@gmail.com>
2019-06-27 17:36:14 +0200

    Render graph using react instead of d3-selection

--- src/graph/ForceGraph.tsx
+++ src/graph/ForceGraph.tsx
@@ -1,13 +1,11 @@
-import "./ForceGraph.css";
-import React, { useEffect, useRef, useMemo, useState } from "react";
 import * as d3Force from "d3-force";
-import { SimulationNodeDatum, SimulationLinkDatum, ForceLink } from "d3-force";
-import * as d3Selection from "d3-selection";
-
+import { SimulationLinkDatum, SimulationNodeDatum } from "d3-force";
+import { keyBy } from "lodash";
+import React, { useMemo } from "react";
+import { colorForScore } from "../colorForScore";
 import { GraphState } from "../state/GraphState";
 import { range } from "../utils";
-import { colorForScore } from "../colorForScore";
-import { keyBy } from "lodash";
+import "./ForceGraph.css";
 
 type NodeDatum = SimulationNodeDatum & {
   nodeName: string;
@@ -73,113 +71,189 @@ export const ForceGraph = ({
   width?: number;
   height?: number;
 }) => {
-  const svgRef = useRef<SVGSVGElement>(null);
-  let [previousGraphData, setPreviousGraphData] = useState<GraphData | null>(
-    null
-  );
-
-  const graphData = useMemo(
-    () => buildGraphData(graphState, previousGraphData),
-    // previousGraphData is omitied on purpose here... This is a code smell but I can't figure it out
-    [graphState]
-  );
-  if (previousGraphData !== graphData) {
-    setPreviousGraphData(graphData);
-  }
-
-  const svgElement: SVGSVGElement | null = svgRef.current;
-  useEffect(() => {
-    if (svgElement === null) {
-      return;
+  const { simulation, forceCenter, forceLink } = useMemo(() => {
+    const nodes: NodeDatum[] = [];
+    const links: LinkDatum[] = [];
+    const useDecayingLinkStength = false;
+    const forceLink = d3Force
+      .forceLink<NodeDatum, LinkDatum>()
+      .id((node: NodeDatum) => node.nodeName);
+    // SHould we use a decaying strength instead
+    if (useDecayingLinkStength) {
+      forceLink
+        .distance(() => 10)
+        .strength(link => (link.score * link.score) / (100 * 100));
+    } else {
+      forceLink.distance(link => 30 + (1 - link.score / 100) * 100);
     }
+    const forceCenter = d3Force.forceCenter();
     const simulation = d3Force
       .forceSimulation<NodeDatum, LinkDatum>()
-      .force(
-        "link",
-        d3Force
-          .forceLink()
-          .id((node: SimulationNodeDatum) => (node as NodeDatum).nodeName)
-          .distance(
-            (link: any) => 30 + (1 - (link as LinkDatum).score / 100) * 100
-          )
-      )
+      .force("link", forceLink)
       .force("collide", d3Force.forceCollide(nodeRadius * 1.5))
-      .force("charge", d3Force.forceManyBody().strength(() => -10))
-      .force("center", d3Force.forceCenter(width / 2, height / 2));
+      .force("charge", d3Force.forceManyBody().strength(() => 15))
+      .force("center", forceCenter)
+      .stop()
+      .nodes(nodes);
+    forceLink.links(links);
+    return { simulation, forceCenter, forceLink };
+  }, []);
 
-    const svgSel = d3Selection.select(svgElement);
-    const linkSel = svgSel
-      .append("g")
-      .attr("class", "ForceGraph__Links")
-      .selectAll("line")
-      .data(graphData.links)
-      .enter()
-      .append("line")
-      .attr("stroke", (d: LinkDatum) => colorForScore(d.score));
-    linkSel.append("title").text(function(d: LinkDatum) {
-      return d.edgeId + ": " + d.score;
-    });
+  const { updatedNodes, updatedLinks } = useMemo(() => {
+    // Update simulation
+    const previousNodes: NodeDatum[] = simulation.nodes();
+    const previousLinks: LinkDatum[] = forceLink.links();
+    const { nodes: updatedNodes, links: updatedLinks } = buildGraphData(
+      graphState,
+      { nodes: previousNodes, links: previousLinks }
+    );
+    // Reconfigure simulation
+    forceCenter.x(width / 2);
+    forceCenter.y(height / 2);
+    simulation.nodes(updatedNodes);
+    forceLink.links(updatedLinks);
 
-    const nodeGroupSel = svgSel
-      .append("g")
-      .attr("class", "ForceGraph__Nodes")
-      .selectAll("g")
-      .data(graphData.nodes)
-      .enter()
-      .append("g")
-      .attr("class", "ForceGraph__NodeGroup");
+    // Run simulation
+    simulation.alpha(1);
+    for (var i = 0; i < 300; ++i) simulation.tick();
+    console.log("nodes", updatedNodes);
+    console.log("links", updatedLinks);
 
-    nodeGroupSel
-      .append("circle")
-      .attr("class", "ForceGraph__NodeCircle")
-      .attr("cx", nodeRadius / 2)
-      .attr("cy", nodeRadius / 2)
-      .attr("r", nodeRadius);
+    return { updatedNodes, updatedLinks };
+  }, [simulation, graphState, height, width, forceCenter, forceLink]);
 
-    nodeGroupSel
-      .append("text")
-      .attr("class", "ForceGraph__NodeText")
-      .attr("x", nodeRadius / 2)
-      .attr("y", nodeRadius / 2)
-      .text(d => d.nodeName);
-
-    nodeGroupSel.append("title").text(d => d.nodeName);
-    //.call(createDragCall(simulation))
-
-    simulation.nodes(graphData.nodes).on("tick", ticked);
-    const forceLink = simulation.force("link") as d3Force.ForceLink<
-      NodeDatum,
-      LinkDatum
-    >;
-    forceLink.links(graphData.links);
-    function ticked() {
-      linkSel
-        .attr("x1", function(d: LinkDatum) {
-          return (d.source as NodeDatum).x as number;
-        })
-        .attr("y1", function(d: LinkDatum) {
-          return (d.source as NodeDatum).y as number;
-        })
-        .attr("x2", function(d: LinkDatum) {
-          return (d.target as NodeDatum).x as number;
-        })
-        .attr("y2", function(d: LinkDatum) {
-          return (d.target as NodeDatum).y as number;
-        });
-
-      nodeGroupSel.attr("transform", function(d: NodeDatum) {
-        return `translate(${(d.x as number) -
-          nodeRadius / 2},${(d.y as number) - nodeRadius / 2})`;
-      });
-    }
-    return () => {
-      simulation.stop();
-      while (svgElement.firstChild) {
-        svgElement.removeChild(svgElement.firstChild);
-      }
-      // TODO remove previous nodes?
-    };
-  }, [width, height, svgElement, graphData]);
-
-  return <svg width={width} height={height} ref={svgRef}></svg>;
+  return (
+    <svg width={width} height={height}>
+      <g className="ForceGraph__Links">
+        {updatedLinks.map(link => (
+          <line
+            key={link.edgeId}
+            className="ForceGraph__Link"
+            stroke={colorForScore(link.score)}
+            x1={(link.source as NodeDatum).x as number}
+            y1={(link.source as NodeDatum).y as number}
+            x2={(link.target as NodeDatum).x as number}
+            y2={(link.target as NodeDatum).y as number}
+          />
+        ))}
+      </g>
+      <g className="ForceGraph__Nodes">
+        {updatedNodes.map(node => (
+          <g
+            key={node.nodeName}
+            className="ForceGraph__NodeGroup"
+            transform={`translate(${(node.x as number) -
+              nodeRadius / 2},${(node.y as number) - nodeRadius / 2})`}
+          >
+            <circle
+              className="ForceGraph__NodeCircle"
+              cx={nodeRadius / 2}
+              cy={nodeRadius / 2}
+              r={nodeRadius}
+            ></circle>
+            <text
+              className="ForceGraph__NodeText"
+              x={nodeRadius / 2}
+              y={nodeRadius / 2}
+            >
+              {node.nodeName}
+            </text>
+            <title>{node.nodeName}</title>
+          </g>
+        ))}
+      </g>
+    </svg>
+  );
 };
+
+// nodes.fo;
+// const svgRef = useRef<SVGSVGElement>(null);
+// let [previousGraphData, setPreviousGraphData] = useState<GraphData | null>(
+//   null
+// );
+
+// const graphData = useMemo(
+//   () => buildGraphData(graphState, previousGraphData),
+//   // previousGraphData is omitied on purpose here... This is a code smell but I can't figure it out
+//   [graphState]
+// );
+// if (previousGraphData !== graphData) {
+//   setPreviousGraphData(graphData);
+// }
+
+// const svgElement: SVGSVGElement | null = svgRef.current;
+// useEffect(() => {
+//   if (svgElement === null) {
+//     return;
+//   }
+
+//   forceLink.links(graphData.links);
+//   for (var i = 0; i < 300; ++i) simulation.tick();
+
+//   const svgSel = d3Selection.select(svgElement);
+//   const linkSel = svgSel
+//     .append("g")
+//     .attr("class", "ForceGraph__Links")
+//     .selectAll("line")
+//     .data(graphData.links)
+//     .enter()
+//     .append("line")
+//     .attr("stroke", (d: LinkDatum) => colorForScore(d.score));
+//   linkSel.append("title").text(function(d: LinkDatum) {
+//     return d.edgeId + ": " + d.score;
+//   });
+
+//   const nodeGroupSel = svgSel
+//     .append("g")
+//     .attr("class", "ForceGraph__Nodes")
+//     .selectAll("g")
+//     .data(graphData.nodes)
+//     .enter()
+//     .append("g")
+//     .attr("class", "ForceGraph__NodeGroup");
+
+//   nodeGroupSel
+//     .append("circle")
+//     .attr("class", "ForceGraph__NodeCircle")
+//     .attr("cx", nodeRadius / 2)
+//     .attr("cy", nodeRadius / 2)
+//     .attr("r", nodeRadius);
+
+//   nodeGroupSel
+//     .append("text")
+//     .attr("class", "ForceGraph__NodeText")
+//     .attr("x", nodeRadius / 2)
+//     .attr("y", nodeRadius / 2)
+//     .text(d => d.nodeName);
+
+//   nodeGroupSel.append("title").text(d => d.nodeName);
+//   //.call(createDragCall(simulation))
+
+//   function ticked() {
+//     linkSel
+//       .attr("x1", function(d: LinkDatum) {
+//         return (d.source as NodeDatum).x as number;
+//       })
+//       .attr("y1", function(d: LinkDatum) {
+//         return (d.source as NodeDatum).y as number;
+//       })
+//       .attr("x2", function(d: LinkDatum) {
+//         return (d.target as NodeDatum).x as number;
+//       })
+//       .attr("y2", function(d: LinkDatum) {
+//         return (d.target as NodeDatum).y as number;
+//       });
+
+//     nodeGroupSel.attr("transform", function(d: NodeDatum) {
+//       return `translate(${(d.x as number) -
+//         nodeRadius / 2},${(d.y as number) - nodeRadius / 2})`;
+//     });
+//   }
+//   return () => {
+//     simulation.stop();
+//     while (svgElement.firstChild) {
+//       svgElement.removeChild(svgElement.firstChild);
+//     }
+//     // TODO remove previous nodes?
+//   };
+// }, [width, height, svgElement, graphData]);
